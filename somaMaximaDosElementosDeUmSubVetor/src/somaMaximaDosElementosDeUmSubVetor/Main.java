455c6364187be62af508702e0bb7b120455af369
Jairo-GitHub-Principal <jairocesa@outlook.com.br>
2024-04-20 01:27:05 -0300

    correção no programa da soma maxima de um subvetor, pois o mesmo não retornava valores menor que 0, isso foi corrgido, iniciando a variavel somamaxima para um valor menor que 0

--- somaMaximaDosElementosDeUmSubVetor/src/somaMaximaDosElementosDeUmSubVetor/Main.java
+++ somaMaximaDosElementosDeUmSubVetor/src/somaMaximaDosElementosDeUmSubVetor/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		
 		
 				// TODO Auto-generated method stub
-			double valoresDoVetorPrincipal [];
+			int valoresDoVetorPrincipal [];
 			int tamanhoDovetorPrincipal, tamanhoDoSubVetor = 0  ;
 			
 			
@@ -25,20 +25,20 @@ public class Main {
 			// aqui sera recebido o valor que definira o tamanho do vetor, pois o tamanho do vetor representara a quantidade de medições que sera feito da temperatura
 			System.out.println("\n informea o tamanho do veotor principal");
 			tamanhoDovetorPrincipal = entrada.nextInt();
-			valoresDoVetorPrincipal = new double[tamanhoDovetorPrincipal];
+			valoresDoVetorPrincipal = new int[tamanhoDovetorPrincipal];
 			
 			System.out.println("agora informe os valores de que sera armazenado no vetor principal, pode ser positivos ou negativos ou ambos");
 			
 			for(int i = 0; i < tamanhoDovetorPrincipal;i++) {
 				System.out.println("informe o valor do indic "+ i);
-				valoresDoVetorPrincipal[i]=entrada.nextDouble();
+				valoresDoVetorPrincipal[i]=entrada.nextInt();
 			}
 			
 	System.out.println("\n informe o tamanho do sub vetor");
 			
 			 tamanhoDoSubVetor  = entrada.nextInt();
 			
-			double result =  somamaxSubVet.SomaMaximaSubVet(tamanhoDoSubVetor,valoresDoVetorPrincipal);
+			int result =  somamaxSubVet.SomaMaximaSubVet(tamanhoDoSubVetor,valoresDoVetorPrincipal);
 			
 			
 			System.out.println("o retorno da função SomaMaximaSubVet para a soma maxima de um sub vetor é = "+ result);
--- somaMaximaDosElementosDeUmSubVetor/src/somaMaximaDosElementosDeUmSubVetor/SomaMaxima.java
+++ somaMaximaDosElementosDeUmSubVetor/src/somaMaximaDosElementosDeUmSubVetor/SomaMaxima.java
@@ -3,15 +3,17 @@ package somaMaximaDosElementosDeUmSubVetor;
 public class SomaMaxima {
 	
 	
-		public  double SomaMaximaSubVet( int tamanhoSubVetor,double valoresDovetPrincipal[]) { // metodo que rretorna um array
+		public  int SomaMaximaSubVet( int tamanhoSubVetor,int valoresDovetPrincipal[]) { // metodo que rretorna um array
 					
 			 // Vetor para armazenar os resultados da média móvel
 				       
-			double [] vetorDosValoresSomados = new double[valoresDovetPrincipal.length - tamanhoSubVetor + 1];
+			int [] vetorDosValoresSomados = new int[valoresDovetPrincipal.length - tamanhoSubVetor + 1];
 			
-			double somaMax=0;
-			double somaMaxDeRetorno = 0;
-			double somaProvisoriaDeSaida= 0;
+			int somaMax=Integer.MIN_VALUE; // essa inicialização da variavel, permiti que ela aceite valores menor que 0, ja que ela vai ser usada em um teste, pra achar o maior valor, e o retultado maior valor pode ser menor que 0, e isso dependendo do teste pode causar um erro, 
+			// exemplo if(somaMax < -5){somaMax = -5} porem nesse teste somaMax vale 0, que é maior que -5 então ela não faria essa atribuição de valor de -5 a somaMax
+			
+			int somaMaxDeRetorno = 0;
+			int somaProvisoriaDeSaida= 0;
 			
 			 
 			
@@ -21,12 +23,15 @@ public class SomaMaxima {
 	       
 	     
 	        // Calculando a média móvel para cada posição no vetor
+			
 	        for (int i = 0; i <= valoresDovetPrincipal.length - tamanhoSubVetor; i++) {
-	            double soma = 0; // essa linha sempre vai zerar a variavel soma, caso ela ja tenha algum valor, antes dela entrar no laço do for com j
+	            int soma = 0; // essa linha sempre vai zerar a variavel soma, caso ela ja tenha algum valor, antes dela entrar no laço do for com j
 	            
 	            for (int j = i; j < i + tamanhoSubVetor; j++) { // obs: j sempre começa valendo i, mais durante a iteração no for(2) ou  for com j, j vai mudar de valor e i vai continuar com o valor definido na iteração do primeiro for até que o loop desse laço  do primeiro for se conclua, assim havera a iteração em i e acontecera i++ e i mudara de valor
+	            	System.out.println("o valor do veotor na posição "+ j + "=" + valoresDovetPrincipal[j] );
 	            	
 	                soma += valoresDovetPrincipal[j]; // fara a soma dos dos valores do do vetor principal,de acordo com a quantidade de indice do subvetor e o resultado sera armazenado na variavel soma
+	                System.out.println("o valor da variavel soma na iteração J = "+j+" = " + soma);
 	            }
 	            vetorDosValoresSomados[i] = soma ; // o resultado da soma sera armazenado na posição i do vetor
 	            
@@ -38,6 +43,7 @@ public class SomaMaxima {
 				
 				
 	        	somaProvisoriaDeSaida = vetorDosValoresSomados[i];
+	        	System.out.println("o valor da varivel somaProvisoriaDeSaida  na iteração i = " +i+" do teste para encontrar a soma maxima = " + somaProvisoriaDeSaida);
 		
 					
 				
@@ -52,7 +58,7 @@ public class SomaMaxima {
 							
 			}
 	       
-	        somaMaxDeRetorno = Math.floor(somaMax);
+	        somaMaxDeRetorno = somaMax;
 	        return somaMaxDeRetorno;
 			
 			
